dec05c90e6e29007a9719de9249e5d40bb1f13f0
Júlia Curto <162455610+curtojulia@users.noreply.github.com>
2024-11-08 16:14:45 -0300

    Add files via upload

--- Semana8-JS/codigo.js
+++ Semana8-JS/codigo.js
@@ -11,6 +11,27 @@ const pega_json = async (caminho) => {   //os dados so vao aparecer no programa 
 
 const conteiner = document.getElementById("conteiner");
 
+const manipulaClick = (e) => {
+    console.log(e.currentTarget);  // target é um elemento
+    const id = e.currentTarget.dataset.id  // esse é o article
+    const url =` detalhes.html?id=${id}`;
+
+    document.cookie = `id=${id}`;
+    document.cookie = `altura=${e.currentTarget.dataset.altura}`;
+
+    //localStorage
+    localStorage.setItem('id', id)
+    localStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset));  // representacao em string 
+
+
+    //sessionstorage
+    sessionStorage.setItem('dados', JSON.stringify(e.currentTarget.dataset));
+
+
+    window.location = url; // pra ir para url indicada 
+} 
+
+
 
 const montaCard = (atleta) =>{
     const cartao = document.createElement("div");
@@ -30,14 +51,20 @@ const montaCard = (atleta) =>{
     descri.innerHTML = atleta.detalhes;
     cartao.appendChild(descri);
 
-    link.innerHTML = "Saiba Mais";
-    link.href = `detalhes.html?id=${atleta.id}`;
-    cartao.appendChild(link)
+    // link.innerHTML = "Saiba Mais";
+    // link.href = `detalhes.html?id=${atleta.id}`;
+    // cartao.appendChild(link)
+
+cartao.onclick = manipulaClick;
+
+cartao.dataset.id = atleta.id;  
+cartao.dataset.nJogos = atleta.n_jogos;
+cartao.dataset.altura = atleta.altura;
 
     return cartao;
 };
 
-pega_json(`${url}feminino`).then( 
+pega_json(`${url}masculino`).then( 
     (r) => {
         r.forEach(
              (ele) => conteiner.appendChild(montaCard(ele))  // loop
@@ -49,4 +76,16 @@ pega_json(`${url}26`).then( (r) => console.log(r))
 
 console.log("Isso imprime primeiro.")
 
+const manipulaBotao = () => {          // para criar a senha, logar e deslogar
+    const texto = document.getElementById('senha').value;
+    if (hex_md5(texto) === "5029cc9dd0295ded2f500084635c18c1"){
+        sessionStorage.setItem("logado", "sim");
 
+    } else {
+        alert("Você errou a senha!");
+    }
+}
+
+ document.getElementById('botao').onclick = manipulaBotao;
+
+ document.getElementById('logout').onclick = () => sessionStorage.removeItem('logado');
--- Semana8-JS/detalhes.js
+++ Semana8-JS/detalhes.js
@@ -1,28 +1,32 @@
 const params = new URLSearchParams(window.location.search)
 const id = params.get("id");
 
+
 const pega_json = async (caminho) => {   //os dados so vao aparecer no programa depois disso tudo abaixo
     const resposta = await fetch(caminho); 
     const dados = await resposta.json();
     return dados;                         // essa funcao vai na api e pega os dados
 };
 
-const div = document.getElementById("div");
-
 const montaPagina = (dados) => {
     const body = document.body;
     body.innerHTML = "";
 
+    
     const nome = document.createElement("h1");
     nome.innerHTML = dados.nome;
     body.appendChild(nome);
-
+    
     const imagem = document.createElement("img");
     imagem.src = dados.imagem;
     body.appendChild(imagem);
+    
+    const descri = document.createElement("p");
+    descri.innerHTML = dados.detalhes;
+    body.appendChild(descri);
 
     const n_jogos = document.createElement("p");
-    n_jogos.innerHTML = `Número de Jogos: ${dados.n_jogos}`;
+    n_jogos.innerHTML = dados.n_jogos;
     body.appendChild(n_jogos);
 
     const posicao = document.createElement("p");
@@ -40,12 +44,29 @@ const montaPagina = (dados) => {
     const no_botafogo_desde = document.createElement("p");
     no_botafogo_desde.innerHTML =  `Está no Botafogo desde: ${dados.no_botafogo_desde}`;
     body.appendChild(no_botafogo_desde);
+};
 
+if(sessionStorage.getItem('logado')){
+
+    
+    pega_json(`https://botafogo-atletas.mange.li/2024-1/${id}`).then(
+        (r) => montaPagina(r)
+        );
+} else {
+    document.body.innerHTML = '<h1>Você precisa estar logado para acessar essa página.</h1>';
 }
 
+const achacookie = (chave) => {   // parte que ajusta os detalhes so aparecerem se estiver logado                       
+    const lista = document.cookie.split("; ");
+    const par = lista.find(
+        (e) => e.startsWith(`${chave}=`)
+    )
+    return par.split("=")[1]
 
+};
+console.log('altura:', achacookie('altura'));
 
-pega_json(`https://botafogo-atletas.mange.li/2024-1/${id}`).then(
-    (r) => montaPagina(r)
-);
+const dadosSessionStorage = sessionStorage.getItem('dados');
+const obj = JSON.parse(dadosSessionStorage);
 
+console.log('número de jogos:', obj.nJogos);
